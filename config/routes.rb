ee91edb29e88b92a505cb0560f0e28a7be08025c
RuhmUndAnsehen <97001540+RuhmUndAnsehen@users.noreply.github.com>
2023-05-16 12:57:02 +0200

    Restrict currencies and countries to #index and #show.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :countries, param: :alpha3_code
-  resources :currencies, param: :alphabetic_code
+  scope only: %i[index show] do
+    resources :countries, param: :alpha3_code
+    resources :currencies, param: :alphabetic_code
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
