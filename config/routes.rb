525840508ed373cb133a92d09e2e980c1b95fd2e
RuhmUndAnsehen <97001540+RuhmUndAnsehen@users.noreply.github.com>
2023-05-16 14:04:13 +0200

    Redirect country and currency alphabetic codes that aren't written in entirely lowercase to the uppercase equivalent.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
-  scope only: %i[index show] do
+  scope abc: /[[:upper:]]{3}/, only: %i[index show] do
     resources :countries, param: :abc
     resources :currencies, param: :abc
   end
+
+  # Redirect country and currency alphabetic codes that aren't written in
+  # entirely lowercase to the uppercase equivalent.
+  # TODO: Determine if the redirect can be done in a way that feels less hacky.
+  scope abc: /[[:alpha:]]{3}/,
+        to: redirect { |p, r| "#{r.fullpath.match(/\A\/[^\/]*/)}/#{p[:abc].upcase}" } do
+    get '/countries/:abc'
+    get '/currencies/:abc'
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
