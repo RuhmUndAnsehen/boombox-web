24975e0085827b2f0dabe6ce3045adad7a8911f9
RuhmUndAnsehen <97001540+RuhmUndAnsehen@users.noreply.github.com>
2023-05-18 17:08:08 +0200

    Add concern Asset.

---
+++ app/models/concerns/asset.rb
@@ -0,0 +1,12 @@
+module Asset
+  extend ActiveSupport::Concern
+
+  included do
+    has_many :asset_pairs, as: :asset, dependent: :destroy
+    has_many :base_assets, through: :asset_pairs, source: :counter_asset
+    has_many :counter_assets, through: :asset_pairs, source: :base_asset
+  end
+
+  def in(counter_asset) = AssetPair.of(self).in(counter_asset)
+  def of(base_asset)    = AssetPair.in(self).of(base_asset)
+end
