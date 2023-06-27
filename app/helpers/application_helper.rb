0713ea5024be2529a5db541a4bdbb39b80bdd0a3
RuhmUndAnsehen <97001540+RuhmUndAnsehen@users.noreply.github.com>
2023-06-27 12:20:15 +0200

    Decorate #form_with to always use the FormBuilder introduced in eabd3e1679e612003514949ecf361aab27f5092e.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
+require 'enhanced/form_builder'
+
 # :nodoc:
 module ApplicationHelper
+  ##
+  # Decorates the built-in #form_with by switching the default form builder with
+  # Enhanced::FormBuilder.
+  def form_with(*, **opts, &)
+    super(*, **{ builder: Enhanced::FormBuilder }.merge(opts), &)
+  end
 end
