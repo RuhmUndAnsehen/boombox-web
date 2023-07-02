RuhmUndAnsehen/boombox-web

commit 8dca8186ebff7a2162ec0085eade4de397d968fb

Optimize DB indices for ORDER performance.

db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_10_132055) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_18_160512) do
   create_table "asset_pairs", force: :cascade do |t|
     t.string "base_asset_type", null: false
     t.integer "base_asset_id", null: false
@@ -38,8 +38,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_10_132055) do
   create_table "countries_currencies", id: false, force: :cascade do |t|
     t.integer "country_id", null: false
     t.integer "currency_id", null: false
-    t.index ["country_id"], name: "index_countries_currencies_on_country_id"
-    t.index ["currency_id"], name: "index_countries_currencies_on_currency_id"
+    t.index ["country_id", "currency_id"], name: "index_countries_currencies_on_country_id_and_currency_id"
+    t.index ["currency_id", "country_id"], name: "index_countries_currencies_on_currency_id_and_country_id"
   end
 
   create_table "currencies", force: :cascade do |t|
@@ -50,8 +50,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_10_132055) do
     t.boolean "active"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["alphabetic_code"], name: "index_currencies_on_alphabetic_code"
-    t.index ["numeric_code"], name: "index_currencies_on_numeric_code"
+    t.index ["alphabetic_code", "active"], name: "index_currencies_on_alphabetic_code_and_active"
+    t.index ["numeric_code", "active"], name: "index_currencies_on_numeric_code_and_active"
   end
 
   create_table "equities", force: :cascade do |t|
@@ -59,6 +59,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_10_132055) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_equities_on_name"
     t.index ["symbol"], name: "index_equities_on_symbol"
   end
 
@@ -71,13 +72,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_10_132055) do
     t.datetime "updated_at", null: false
     t.string "type", null: false
     t.float "exchange_rate", null: false
-    t.index ["asset_pair_id", "observed_at"], name: "index_exchange_rates_on_asset_pair_id_and_observed_at"
-    t.index ["asset_pair_id", "type", "observed_at", "exchange_rate"], name: "index_exchange_rates_on_all_columns"
-    t.index ["asset_pair_id", "type", "observed_at"], name: "index_exchange_rates_on_asset_pair_id_and_type_and_observed_at"
     t.index ["asset_pair_id"], name: "index_exchange_rates_on_asset_pair_id"
-    t.index ["exchange_rate"], name: "index_exchange_rates_on_exchange_rate"
-    t.index ["observed_at"], name: "index_exchange_rates_on_observed_at"
-    t.index ["type", "observed_at"], name: "index_exchange_rates_on_type_and_observed_at"
+    t.index ["exchange_rate", "type", "asset_pair_id"], name: "index_exchange_rates_on_exchange_rate_and_type_and_asset_pair_id"
+    t.index ["observed_at", "exchange_rate", "type", "asset_pair_id"], name: "index_exchange_rates_on_all_columns"
+    t.index ["observed_at", "type", "asset_pair_id"], name: "index_exchange_rates_on_observed_at_and_type_and_asset_pair_id"
+    t.index ["type", "observed_at", "asset_pair_id"], name: "index_exchange_rates_on_type_and_observed_at_and_asset_pair_id"
     t.index ["type"], name: "index_exchange_rates_on_type"
   end
 
@@ -89,7 +88,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_10_132055) do
     t.datetime "updated_at", null: false
     t.string "timezone"
     t.index ["country_id"], name: "index_exchanges_on_country_id"
-    t.index ["symbol"], name: "index_exchanges_on_symbol", unique: true
+    t.index ["name", "country_id"], name: "index_exchanges_on_name_and_country_id"
+    t.index ["symbol", "country_id"], name: "index_exchanges_on_symbol_and_country_id", unique: true
   end
 
   create_table "options", force: :cascade do |t|
@@ -111,8 +111,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_10_132055) do
     t.string "security_type", null: false
     t.integer "security_id", null: false
     t.integer "exchange_id", null: false
-    t.index ["exchange_id"], name: "index_security_listings_on_exchange_id"
-    t.index ["security_type", "security_id"], name: "index_security_listings_on_security"
+    t.index ["exchange_id", "security_type", "security_id"], name: "index_security_listings_on_exchange_and_security"
+    t.index ["security_type", "security_id", "exchange_id"], name: "index_security_listings_on_security_and_exchange"
   end
 
   add_foreign_key "exchange_rates", "asset_pairs"

db/migrate/20230618160512_optimize_indices.rb
@@ -1,0 +1,62 @@
+# frozen_string_literal: true
+
+##
+# I'm not satisfied with how I constructed indices in previous migrations.
+# This one attempts to fix that. Indices are now built with ORDER performance in
+# mind.
+class OptimizeIndices < ActiveRecord::Migration[7.0]
+  def change
+    change_table :countries_currencies do |t|
+      t.remove_index :country_id
+      t.remove_index :currency_id
+
+      t.index %i[country_id currency_id]
+      t.index %i[currency_id country_id]
+    end
+
+    change_table :currencies do |t|
+      t.remove_index :alphabetic_code
+      t.remove_index :numeric_code
+
+      t.index %i[alphabetic_code active]
+      t.index %i[numeric_code active]
+    end
+
+    change_table :equities do |t|
+      t.index :name
+    end
+
+    change_table :exchange_rates do |t|
+      t.remove_index %i[asset_pair_id observed_at]
+      t.remove_index %i[asset_pair_id type observed_at exchange_rate],
+                     name: 'index_exchange_rates_on_all_columns'
+      t.remove_index %i[asset_pair_id type observed_at]
+      t.remove_index :exchange_rate
+      t.remove_index :observed_at
+      t.remove_index %i[type observed_at]
+
+      t.index %i[exchange_rate type asset_pair_id]
+      t.index %i[observed_at type asset_pair_id]
+      t.index %i[type observed_at asset_pair_id]
+      t.index %i[observed_at exchange_rate type asset_pair_id],
+              name: 'index_exchange_rates_on_all_columns'
+    end
+
+    change_table :exchanges do |t|
+      t.remove_index :symbol, unique: true
+
+      t.index %i[name country_id]
+      t.index %i[symbol country_id], unique: true
+    end
+
+    change_table :security_listings do |t|
+      t.remove_index :exchange_id
+      t.remove_index %i[security_type security_id]
+
+      t.index %i[exchange_id security_type security_id],
+              name: 'index_security_listings_on_exchange_and_security'
+      t.index %i[security_type security_id exchange_id],
+              name: 'index_security_listings_on_security_and_exchange'
+    end
+  end
+end

commit 2144437f3600a925f8af582e9eb561a232f98326

Reestablish proper exchanges unique indexing broken by 8dca8186ebff7a2162ec0085eade4de397d968fb.

db/migrate/20230701221254_fix_exchange_indices.rb
@@ -1,0 +1,13 @@
+# frozen_string_literal: true
+
+# Undoes the change that removed uniqueness from the #symbol column and instead
+# made the symbol-country combination unique.
+class FixExchangeIndices < ActiveRecord::Migration[7.0]
+  def change
+    change_table :exchanges do |t|
+      t.remove_index %i[symbol country_id], unique: true
+
+      t.index :symbol, unique: true
+    end
+  end
+end

db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_18_160512) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_01_221254) do
   create_table "asset_pairs", force: :cascade do |t|
     t.string "base_asset_type", null: false
     t.integer "base_asset_id", null: false
@@ -89,7 +89,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_18_160512) do
     t.string "timezone"
     t.index ["country_id"], name: "index_exchanges_on_country_id"
     t.index ["name", "country_id"], name: "index_exchanges_on_name_and_country_id"
-    t.index ["symbol", "country_id"], name: "index_exchanges_on_symbol_and_country_id", unique: true
+    t.index ["symbol"], name: "index_exchanges_on_symbol", unique: true
   end
 
   create_table "options", force: :cascade do |t|