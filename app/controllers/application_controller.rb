4bb6e876a005000c47bb6d64d068abbc8ac15c32
RuhmUndAnsehen <97001540+RuhmUndAnsehen@users.noreply.github.com>
2023-08-23 13:37:13 +0200

    Add ApplicationController::model_name.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@
 
 # :nodoc:
 class ApplicationController < ActionController::Base
+  class << self
+    ##
+    # Returns the stringyfied class name of the ActiveRecord model that should
+    # be associated with this controller.
+    #
+    # By default, this is the demodulized controller name (without the
+    # "Controller"), singularized.
+    #
+    # Does not check if the model class is actually defined.
+    def model_name = name.demodulize[..-11].singularize
+  end
 end
