456946d33ea045e2f4c154b93d9186c1385d87e6
RuhmUndAnsehen <97001540+RuhmUndAnsehen@users.noreply.github.com>
2023-09-03 17:10:38 +0200

    Override MarketData::EquitiesController::find_asset_pair.
    It now accepts the same Exchange:Equity format that EquitiesController::find_equity does.
    On integer ids, the AssetPair with that id is returned, not the one matching the Equity with that id.

--- app/controllers/market_data/equities_controller.rb
+++ app/controllers/market_data/equities_controller.rb
@@ -3,4 +3,13 @@
 ##
 # Inherits from AssetPairsController to explicitly handle AssetPairs where
 # #base_asset is of type Equity.
-class MarketData::EquitiesController < AssetPairsController; end
+class MarketData::EquitiesController < AssetPairsController
+  class << self
+    def find_asset_pair(id)
+      return super if int?(id)
+
+      base_asset = ::EquitiesController.find_equity(id)
+      asset_pairs.where(base_asset:).first!
+    end
+  end
+end
