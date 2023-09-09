d01badd54db0cc91c114d5368254646887fc3310
RuhmUndAnsehen <97001540+RuhmUndAnsehen@users.noreply.github.com>
2023-09-09 21:51:54 +0200

    Fix AssetPair::Equity#to_param not working as intended.

--- app/models/asset_pair/equity.rb
+++ app/models/asset_pair/equity.rb
@@ -4,4 +4,8 @@
 # Class enabling the rendering of AssetPairs in a dedicated
 # equity controller while maintaining the ability to manage
 # AssetPairs regardless of type in the AssetPairsController.
-class AssetPair::Equity < AssetPair; end
+class AssetPair::Equity < AssetPair
+  class << self
+    def preload_all(...) = super.preload(base_asset: :exchange)
+  end
+end
