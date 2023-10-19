3b399873e97dfe57114f3e0be634016cfbc5a846
RuhmUndAnsehen <97001540+RuhmUndAnsehen@users.noreply.github.com>
2023-10-19 20:42:07 +0200

    Instead of ...::tag_names defining the methods in the including class, they are now defined on a module that is then included.

--- lib/helpers/model_output/builder.rb
+++ lib/helpers/model_output/builder.rb
@@ -9,6 +9,31 @@ module Helpers::ModelOutput
     extend ActiveSupport::Concern
     include ::Helpers::Builder
 
+    ##
+    # When included, defines tag name methods. The methods are inserted in the
+    # inheritance hierarchy and can be overriden in the including class.
+    class TagNameDefinitions < Module
+      def initialize(tags_with_defaults) # rubocop:disable Metrics/MethodLength
+        super()
+
+        tags_with_defaults.transform_keys! { |tag| :"#{tag}_tag_name" }
+
+        assignments = tags_with_defaults.map do |tag, default|
+          "self.#{tag} = tag_names.fetch(#{tag.inspect}, #{default.inspect})"
+        end
+
+        module_eval <<-RUBY, __FILE__, __LINE__ + 1
+          attr_accessor #{tags_with_defaults.keys.map(&:inspect).join(', ')}
+
+          def initialize_tag_names(tag_names)
+            super
+
+            #{assignments.join("\n")}
+          end
+        RUBY
+      end
+    end
+
     class_methods do
       ##
       # Defines attribute accessors and an automatic initializer for HTML tag
@@ -24,26 +49,12 @@ module Helpers::ModelOutput
       #     builder = ExampleBuilder.new(foo_tag_name: :test)
       #     builder.foo_tag_name # => :test
       #     builder.bar_tag_name # => :baz
-      def tag_names( # rubocop:disable Metrics/MethodLength
-        *tags, **tags_with_defaults
-      )
+      def tag_names(*tags, **tags_with_defaults)
         tags_with_defaults.merge!(
           tags.to_h { |obj| obj.is_a?(Array) ? obj : [obj, nil] }
-        ).transform_keys! { |tag| :"#{tag}_tag_name" }
+        )
 
-        assignments = tags_with_defaults.map do |tag, default|
-          "self.#{tag} = tag_names.fetch(#{tag.inspect}, #{default.inspect})"
-        end
-
-        module_eval <<-RUBY, __FILE__, __LINE__ + 1
-          attr_accessor #{tags_with_defaults.keys.map(&:inspect).join(', ')}
-
-          def initialize_tag_names(tag_names)
-            super
-
-            #{assignments.join("\n")}
-          end
-        RUBY
+        include TagNameDefinitions.new(tags_with_defaults)
       end
     end
 
