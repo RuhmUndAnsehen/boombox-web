d6909d6a01102762ffa7a8cd10499ac585e0a78c
RuhmUndAnsehen <97001540+RuhmUndAnsehen@users.noreply.github.com>
2023-10-23 21:33:41 +0200

    Support spawn options in all SpawnHelper methods.

--- lib/helpers/model_output/spawn_helper.rb
+++ lib/helpers/model_output/spawn_helper.rb
@@ -8,20 +8,24 @@ module Helpers::ModelOutput::SpawnHelper
   extend ActiveSupport::Concern
   include ::Helpers::ModelOutput::Builder
 
-  def association_builder(association, &block)
+  def association_builder(association, **, &)
     spawn_as(::Helpers::ModelOutput::AssociationOutputBuilder,
-             association:, &block)
+             association:, **, &)
   end
 
-  def attribute_builder(attribute, &block)
-    spawn_as(::Helpers::ModelOutput::AttributeOutputBuilder, attribute:, &block)
+  def attribute_builder(attribute, **, &)
+    spawn_as(::Helpers::ModelOutput::AttributeOutputBuilder, attribute:, **, &)
   end
 
   def model_builder(model, **opts, &)
-    opts = spawn_options(opts) if respond_to?(:spawn_options)
-
     spawn_as(::Helpers::ModelOutput::ModelOutputBuilder, model:, **opts, &)
   end
 
   def show(...) = model_builder(...).to_s
+
+  def spawn_as(*, **opts, &)
+    opts = spawn_options(opts) if respond_to?(:spawn_options)
+
+    super(*, **opts, &)
+  end
 end
