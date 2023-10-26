0882f217239d8a234ce436d4d956687aef548410
RuhmUndAnsehen <97001540+RuhmUndAnsehen@users.noreply.github.com>
2023-10-26 11:52:58 +0200

    In addition to the field type, generate a CSS class for the type limit in generated model output.

--- lib/helpers/model_output/attribute_output_builder.rb
+++ lib/helpers/model_output/attribute_output_builder.rb
@@ -7,7 +7,7 @@ require_relative 'builder'
 class Helpers::ModelOutput::AttributeOutputBuilder
   include ::Helpers::ModelOutput::Builder
 
-  attr_accessor :index, :link, :name, :raw_name, :type, :value
+  attr_accessor :index, :link, :name, :raw_name, :type, :type_limit, :value
 
   tag_names :container, name: :dt, value: :dd
 
@@ -23,7 +23,8 @@ class Helpers::ModelOutput::AttributeOutputBuilder
   end
 
   def component_options(options)
-    options = add_class(type, to: options) if type.present?
+    options = add_class(type_limit_class, to: options)
+    add_class(type, to: options) if type.present?
 
     add_class('model-attribute', to: options)
   end
@@ -47,14 +48,22 @@ class Helpers::ModelOutput::AttributeOutputBuilder
     content_tag_if_name(container_tag_name, content)
   end
 
+  ##
+  # Returns a CSS class for #type_limit.
+  def type_limit_class
+    type_limit.present? ? "limit-#{type_limit}" : 'unlimited'
+  end
+
   private
 
   def initialize_attribute_attributes(raw_name)
-    klass = parent.model.class
+    klass = model.class
+    type_metadata = klass.columns_hash[raw_name]&.sql_type_metadata
 
     self.name = klass.human_attribute_name(raw_name)
     self.raw_name = raw_name
-    self.type = klass.columns_hash[raw_name]&.sql_type_metadata&.type
-    self.value = parent.model.attributes[raw_name]
+    self.type = type_metadata&.type
+    self.type_limit = type_metadata&.limit
+    self.value = model.attributes[raw_name]
   end
 end
