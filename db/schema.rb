515268ae0705f3754767357e88a92be2275a463d
RuhmUndAnsehen <97001540+RuhmUndAnsehen@users.noreply.github.com>
2024-02-02 21:58:53 +0100

    Upgrade Rails to version 7.1.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_01_221254) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_02_172561) do
   create_table "asset_pairs", force: :cascade do |t|
     t.string "base_asset_type", null: false
     t.integer "base_asset_id", null: false
