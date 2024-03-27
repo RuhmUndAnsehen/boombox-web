2e5c0c126c31a877c4c495b1854ab07fdfbbd0d1
RuhmUndAnsehen <97001540+RuhmUndAnsehen@users.noreply.github.com>
2024-03-27 22:00:55 +0100

    Define SpawnHelper instance methods using the newly added ::builder_spawner and ::builder_spawners methods.

--- lib/helpers/model_output/spawn_helper.rb
+++ lib/helpers/model_output/spawn_helper.rb
@@ -8,18 +8,24 @@ module Helpers::ModelOutput::SpawnHelper
   extend ActiveSupport::Concern
   include ::Helpers::ModelOutput::Builder
 
-  def association_builder(association, **, &)
-    spawn_as(::Helpers::ModelOutput::AssociationOutputBuilder,
-             association:, **, &)
+  class << self
+    def builder_spawner(name, class_name: nil, param: nil)
+      param ||= name
+      class_name ||= "Helpers::ModelOutput::#{name.to_s.classify}OutputBuilder"
+
+      module_eval <<-RUBY, __FILE__, __LINE__ + 1
+        def #{name}_builder(#{param}, **, &)
+          spawn_as(#{name}_builder_class_name.constantize, #{param}:, **, &)
+        end
+
+        def #{name}_builder_class_name = #{class_name.inspect}
+      RUBY
+    end
+
+    def builder_spawners(*names) = names.each(&method(:builder_spawner))
   end
 
-  def attribute_builder(attribute, **, &)
-    spawn_as(::Helpers::ModelOutput::AttributeOutputBuilder, attribute:, **, &)
-  end
-
-  def model_builder(model, **opts, &)
-    spawn_as(::Helpers::ModelOutput::ModelOutputBuilder, model:, **opts, &)
-  end
+  builder_spawners :association, :attribute, :model
 
   def show(...) = model_builder(...).to_s
 
